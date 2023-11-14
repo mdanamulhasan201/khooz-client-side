118c750e37bfe5729b6a7dc433b9b1b21065d76f
Md. Anamul Hasan <mdanamulhasan.cse@gmail.com>
2023-11-14 23:16:43 +0600

    register page design change and footer design change

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { AiOutlineArrowRight, AiOutlineGoogle } from "react-icons/ai";
-import { FaFacebookF } from "react-icons/fa";
+import { FaFacebookF, FaLinkedinIn } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../Shared/Footer";
@@ -55,60 +55,66 @@ const Register = () => {
             }
 
             <div className="py-16">
-                <div className="container mx-auto">
-                    <div className=" mt-4 ">
-                        <div className="w-full flex justify-center items-center p-10">
-                            <div className=" w-[30%] mx-auto shadow-lg rounded-md" >
-                                <div className="p-8 items-center">
-                                    <h2 className="text-center w-full text-xl text-slate-600 font-bold ">Register</h2>
-                                    <div className="">
-                                        <form onSubmit={register}>
-                                            <div className="flex flex-col gap-1 mb-2 ">
-                                                <label htmlFor="name">Name</label>
-                                                <input onChange={inputHandle} value={state.name} type="text" id="name" name="name" placeholder="name" className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-red-400 rounded-md " required />
-                                            </div>
-                                            <div className="flex flex-col gap-1 mb-2 ">
-                                                <label htmlFor="email">Email</label>
-                                                <input onChange={inputHandle} value={state.email} type="email" id="email" name="email" placeholder="email" className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-red-400 rounded-md " required />
-                                            </div>
-                                            <div className="flex flex-col gap-1 mb-5 ">
-                                                <label htmlFor="password">Password</label>
-                                                <input onChange={inputHandle} value={state.password} type="password" id="password" name="password" placeholder="******" className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-red-400 rounded-md " required />
-                                            </div>
-                                            <button className="px-8 w-full py-2 bg-red-400 text-white shadow-lg hover:shadow-red-400/40">Register</button>
+                <div className="max-w-screen-xl mx-auto">
 
-                                            <Link to='/' className="flex gap-1 items-center justify-end">
-                                                <p className="text-md  my-2 text-green-500 font-semibold ">Guest</p>
-                                                <AiOutlineArrowRight className="text-green-500"></AiOutlineArrowRight>
-                                            </Link>
-
-                                        </form>
-
-                                        <div className='flex justify-center items-center py-2'>
-                                            <div className='h-[1px] bg-slate-300 w-[95%]'></div>
-                                            <span className='px-3 text-slate-600'>or</span>
-                                            <div className='h-[1px] bg-slate-300 w-[95%]'></div>
+                    <div className="w-full flex justify-center items-center p-10">
+                        <div className="md:w-[50%] sm:w-[50%] lg:w-[30%] w-full mx-auto shadow-lg rounded-md" >
+                            <div className="p-8 items-center">
+                                <h2 className="text-center w-full text-xl text-slate-600 font-bold ">Register</h2>
+                                <div className="">
+                                    <form onSubmit={register} className="mb-5">
+                                        <div className="flex flex-col gap-1 mb-5 ">
+                                            <label htmlFor="name">Name</label>
+                                            <input onChange={inputHandle} value={state.name} type="text" id="name" name="name" placeholder="name" className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-red-400 rounded-md " required />
                                         </div>
+                                        <div className="flex flex-col gap-1 mb-5 ">
+                                            <label htmlFor="email">Email</label>
+                                            <input onChange={inputHandle} value={state.email} type="email" id="email" name="email" placeholder="email" className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-red-400 rounded-md " required />
+                                        </div>
+                                        <div className="flex flex-col gap-1 mb-5 ">
+                                            <label htmlFor="password">Password</label>
+                                            <input onChange={inputHandle} value={state.password} type="password" id="password" name="password" placeholder="******" className="w-full px-3 py-2 border border-slate-200 outline-0 focus:border-red-400 rounded-md " required />
+                                        </div>
+                                        <button className="px-8 w-full py-2 rounded-md bg-green-400 text-white shadow-lg hover:shadow-red-400/40">Register</button>
 
-                                        <button className='px-8 w-full py-2 bg-blue-500 shadow-lg hover:shadow-indigo-400/40 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
-                                            <span><FaFacebookF /></span>
-                                            <span>Login with Facebook</span>
-                                        </button>
-                                        <button className='px-8 w-full py-2 bg-orange-400 shadow-lg hover:shadow-orange-400/40 text-white rounded-md flex justify-center items-center gap-2 mb-3'>
-                                            <span><AiOutlineGoogle /></span>
-                                            <span>Login with Facebook</span>
-                                        </button>
 
+
+                                    </form>
+
+                                    <div className='flex justify-center items-center  py-5'>
+                                        <div className='h-[1px] bg-slate-300 w-[95%]'></div>
+                                        <span className='px-3 text-slate-600'>or</span>
+                                        <div className='h-[1px] bg-slate-300 w-[95%]'></div>
                                     </div>
-                                    <div className='text-center text-slate-600 pt-1'>
-                                        <p>You have an account ? <Link className='text-blue-500' to='/login'>Login</Link></p>
+
+                                    <div className="flex gap-5 justify-center items-center mb-5">
+                                        <div>
+                                            <button className="transform hover:scale-125 transition-transform duration-500  border-blue-400 border flex justify-center items-center rounded-full w-10 h-10 text-2xl">
+                                                <FaFacebookF className="text-blue-500" />
+                                            </button>
+                                        </div>
+                                        <div>
+                                            <button className="transform hover:scale-125 transition-transform duration-500 border-red-400 border flex justify-center items-center rounded-full w-10 h-10 text-2xl">
+                                                <AiOutlineGoogle className="text-red-400" />
+                                            </button>
+                                        </div>
+                                        <div>
+                                            <button className="transform hover:scale-125 transition-transform duration-500 border-blue-400 border flex justify-center items-center rounded-full w-10 h-10 text-2xl">
+                                                <FaLinkedinIn className="text-blue-500" />
+                                            </button>
+                                        </div>
                                     </div>
+
                                 </div>
-                                <div></div>
+                                <div className='text-center text-slate-600 pt-1'>
+                                    <p>You have an account ? <Link className='text-blue-500' to='/login'>Login</Link></p>
+                                </div>
                             </div>
+                            <div></div>
                         </div>
-
                     </div>
+
+
                 </div>
             </div>
             <Footer></Footer>
